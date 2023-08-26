3865c814937ba2756a9a4db11cabd41bd20c018f
vasilyocheck <vasily.medvedev1986@gmail.com>
2023-08-26 15:40:35 +0300

    create app components

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -5,7 +5,7 @@ export const Profile = () => {
         <div className={'content'}>
             <div>
                 <img
-                    src={'https://t3.ftcdn.net/jpg/01/62/85/00/360_F_162850009_lnHxshkJUH36xjM3Ysi8gwAYwUBOaRWB.jpg'}/>
+                    src={'https://t3.ftcdn.net/jpg/01/62/85/00/360_F_162850009_lnHxshkJUH36xjM3Ysi8gwAYwUBOaRWB.jpg'} alt={'top profile image'}/>
             </div>
             <div>
                 ava + description
