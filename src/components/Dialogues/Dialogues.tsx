e1013e5e0a3dd110dd4e53e2e99859bbf93622d1
vasilyocheck <vasily.medvedev1986@gmail.com>
2023-09-15 06:18:59 +0300

    learn exact route (samurai way 22)

--- src/components/Dialogues/Dialogues.tsx
+++ src/components/Dialogues/Dialogues.tsx
@@ -1,27 +1,28 @@
 import React from 'react';
 import s from './Dialogues.module.css'
+import {NavLink} from "react-router-dom";
 
 export const Dialogues = () => {
     return (
         <div className={s.dialogues}>
             <div className={s.dialoguesItems}>
                 <div className={s.dialogue + ' ' + s.active}>
-                    Dimych
+                    <NavLink to='/dialogues/1'>Dimych</NavLink>
                 </div>
                 <div className={s.dialogue}>
-                    Andrey
+                    <NavLink to='/dialogues/2'>Andrey</NavLink>
                 </div>
                 <div className={s.dialogue}>
-                    Sveta
+                    <NavLink to='/dialogues/3'>Sveta</NavLink>
                 </div>
                 <div className={s.dialogue}>
-                    Sasha
+                    <NavLink to='/dialogues/4'>Sasha</NavLink>
                 </div>
                 <div className={s.dialogue}>
-                    Victor
+                    <NavLink to='/dialogues/4'>Victor</NavLink>
                 </div>
                 <div className={s.dialogue}>
-                    Igor
+                    <NavLink to='/dialogues/6'>Igor</NavLink>
                 </div>
 
             </div>
