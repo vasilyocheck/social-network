627557f9583b18cf7af96865f0e8865e2964e7f1
vasilyocheck <vasily.medvedev1986@gmail.com>
2023-09-15 06:03:07 +0300

    add Dialogues (samurai way 21)

--- src/components/Dialogues/Dialogues.tsx
+++ src/components/Dialogues/Dialogues.tsx
@@ -3,8 +3,33 @@ import s from './Dialogues.module.css'
 
 export const Dialogues = () => {
     return (
-        <div>
-            Dialogues
+        <div className={s.dialogues}>
+            <div className={s.dialoguesItems}>
+                <div className={s.dialogue + ' ' + s.active}>
+                    Dimych
+                </div>
+                <div className={s.dialogue}>
+                    Andrey
+                </div>
+                <div className={s.dialogue}>
+                    Sveta
+                </div>
+                <div className={s.dialogue}>
+                    Sasha
+                </div>
+                <div className={s.dialogue}>
+                    Victor
+                </div>
+                <div className={s.dialogue}>
+                    Igor
+                </div>
+
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi!</div>
+                <div className={s.message}>What about your IT-Kamasutra?</div>
+                <div className={s.message}>Yoh!</div>
+            </div>
         </div>
     );
 };
