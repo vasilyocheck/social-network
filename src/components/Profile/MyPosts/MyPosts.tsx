91061c22a3e9949cab851b4b2760c2d6ade534d9
vasilyocheck <vasily.medvedev1986@gmail.com>
2023-10-07 16:47:41 +0300

    refactor to store OOP (samurai way 37)

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent, useRef} from 'react';
+import React, {ChangeEvent} from 'react';
 import s from "./MyPosts.module.css";
 import {Post} from "./Post/Post";
 import {PostsType} from "../../../redux/state";
