12988136b9e3b71b5cf877febe63c6eb44f94598
vasilyocheck <vasily.medvedev1986@gmail.com>
2023-11-09 06:59:35 +0300

    separate pure functional component users (samurai way 56)

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,60 +1,51 @@
-import React from 'react'
 import s from "./Users.module.css";
 import avatarPlaceholder from "../../assets/img/avatar-placeholder.png";
-import {usersApi, UserType} from "../../api/users-api";
+import React, {FC} from "react";
+import {UserType} from "../../api/users-api";
 
 type UsersPropsType = {
-    users: UserType[]
-    pageSize: number
     totalUsersCount: number
+    pageSize: number
     currentPage: number
+    changePageNumber: (pageNumber: number) => void
+    users: UserType[]
     follow: (userId: number) => void
     unfollow: (userId: number) => void
-    setUsers: (newUsers: UserType[]) => void
-    setCurrentPage: (newCurrentPage:number) => void
-    setTotalUsersCount: (totalCount: number) => void
 }
 
-export class Users extends React.Component<UsersPropsType>{
-    componentDidMount() {
-        if(this.props.users.length < 1) {
-            usersApi.getUsers(this.props.pageSize, this.props.currentPage)
-                .then(res => {
-                    this.props.setUsers(res.data.items);
-                    this.props.setTotalUsersCount(res.data.totalCount);
-                })
-        }
+export const Users: FC<UsersPropsType> = (
+    {
+        totalUsersCount,
+        pageSize,
+        currentPage,
+        changePageNumber,
+        users,
+        follow,
+        unfollow
+    }) => {
+    let pagesCount = Math.ceil(totalUsersCount / pageSize);
+    const pages = [];
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i);
     }
 
-    render() {
-        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
-        const pages = [];
-        for (let i = 1; i <= pagesCount; i++) {
-            pages.push(i);
-        }
-        const handleChangePageNumber = (pageNumber: number) => {
-            this.props.setCurrentPage(pageNumber);
-            usersApi.getUsers(this.props.pageSize, pageNumber)
-                .then(res => this.props.setUsers(res.data.items))
-        }
-
-        return (
+    return (
+        <div>
             <div>
-                <div>
-                    {pages.map(p => {
-                        return (<span key={p}
-                                      className={this.props.currentPage === p ? s.selectedPage : s.page}
-                                        onClick={() => handleChangePageNumber(p) }
-                        >{p}</span>);
+                {pages.map(p => {
+                    return (<span key={p}
+                                  className={currentPage === p ? s.selectedPage : s.page}
+                                  onClick={(e) => changePageNumber(p)}
+                    >{p}</span>);
                 })}
-                </div>
-                {this.props.users.map(u => {
-                    const followUnfollowButton = u.followed
-                        ? <button onClick={() => this.props.unfollow(u.id)}>Unfollow</button>
-                        : <button onClick={() => this.props.follow(u.id)}>Follow</button>;
+            </div>
+            {users.map(u => {
+                const followUnfollowButton = u.followed
+                    ? <button onClick={() => unfollow(u.id)}>Unfollow</button>
+                    : <button onClick={() => follow(u.id)}>Follow</button>;
 
-                    return (
-                        <div key={u.id}>
+                return (
+                    <div key={u.id}>
                         <span>
                             <div className={s.avatarContainer}>
                                 <img src={u.photos.small || avatarPlaceholder} alt={u.name}/>
@@ -63,7 +54,7 @@ export class Users extends React.Component<UsersPropsType>{
                                 {followUnfollowButton}
                             </div>
                         </span>
-                            <span>
+                        <span>
                             <span>
                                 <div>{u.name}</div>
                                 <div>{u.status}</div>
@@ -73,10 +64,9 @@ export class Users extends React.Component<UsersPropsType>{
                                 <div>{'u.location.city'}</div>
                             </span>
                         </span>
-                        </div>
-                    );
-                })}
-            </div>
-        );
-    }
+                    </div>
+                );
+            })}
+        </div>
+    );
 }
--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -8,8 +8,52 @@ import {
     setUsersAC,
     unfollowAC
 } from "../../redux/reducers/users-reducer";
-import {UserType} from "../../api/users-api";
+import {usersApi, UserType} from "../../api/users-api";
 import {Users} from "./Users";
+
+type UsersAPIComponentPropsType = {
+    users: UserType[]
+    pageSize: number
+    totalUsersCount: number
+    currentPage: number
+    follow: (userId: number) => void
+    unfollow: (userId: number) => void
+    setUsers: (newUsers: UserType[]) => void
+    setCurrentPage: (newCurrentPage:number) => void
+    setTotalUsersCount: (totalCount: number) => void
+}
+
+export class UsersAPIComponent extends React.Component<UsersAPIComponentPropsType>{
+    componentDidMount() {
+        if(this.props.users.length < 1) {
+            usersApi.getUsers(this.props.pageSize, this.props.currentPage)
+                .then(res => {
+                    this.props.setUsers(res.data.items);
+                    this.props.setTotalUsersCount(res.data.totalCount);
+                })
+        }
+    }
+    handleChangePageNumber(pageNumber: number){
+        console.log('invoked inside UsersAPIComponent')
+        this.props.setCurrentPage(pageNumber);
+        usersApi.getUsers(this.props.pageSize, pageNumber)
+            .then(res => this.props.setUsers(res.data.items))
+    }
+
+    render() {
+        return (
+            <Users totalUsersCount={this.props.totalUsersCount}
+                   pageSize={this.props.pageSize}
+                   currentPage={this.props.currentPage}
+                   changePageNumber={this.handleChangePageNumber.bind(this)}
+                   users={this.props.users}
+                   follow={this.props.follow}
+                   unfollow={this.props.unfollow}
+            />
+        );
+    }
+}
+
 
 export const UsersContainer = () => {
     const dispatch = useDispatch();
@@ -38,7 +82,7 @@ export const UsersContainer = () => {
     }
 
     return (
-        <Users users={users}
+        <UsersAPIComponent users={users}
                follow={follow}
                unfollow={unfollow}
                setUsers={setUsers}
