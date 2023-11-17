98ba7e2beef2692e7820e0eaa4a13f361d806f9b
vasilyocheck <vasily.medvedev1986@gmail.com>
2023-11-17 08:21:12 +0300

    add profile api (samurai way 59)

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import {Header} from "./components/Header/Header";
 import {NavBar} from "./components/NavBar/NavBar";
-import {Profile} from "./components/Profile/Profile";
 import {Route, Routes} from "react-router-dom";
 import {News} from "./components/News/News";
 import {Music} from "./components/Music/Music";
