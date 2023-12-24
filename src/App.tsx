7daafdde4d8577ca4f2d3c9fb737113a5bb8eb4f
vasilyocheck <vasily.medvedev1986@gmail.com>
2023-12-24 18:15:48 +0300

    add lazy loading (samurai way 94)

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { lazy } from "react";
 import "./App.css";
 import { NavBar } from "components/NavBar/NavBar";
 import { Route, Routes } from "react-router-dom";
 import { News } from "components/News/News";
 import { Music } from "components/Music/Music";
 import { Settings } from "components/Settings/Settings";
-import { DialoguesContainer } from "components/Dialogues/DialoguesContainer";
 import { UsersContainer } from "components/Users/UsersContainer";
-import { ProfileContainer } from "components/Profile/ProfileContainer";
 import { HeaderContainer } from "components/Header/HeaderContainer";
 import { Login } from "components/Login/Login";
 import { WithAuthRedirect } from "hoc/withAuthRedirect";
+import { WithSuspense } from "hoc/withSuspense";
+
+const DialoguesContainer = lazy(() =>
+  import("./components/Dialogues/DialoguesContainer").then(({ DialoguesContainer }) => ({
+    default: DialoguesContainer,
+  })),
+);
+
+const ProfileContainer = lazy(() =>
+  import("./components/Profile/ProfileContainer").then(({ ProfileContainer }) => ({
+    default: ProfileContainer,
+  })),
+);
 
 const App = () => {
   return (
@@ -19,9 +30,9 @@ const App = () => {
       <NavBar />
       <div className="app-wrapper-content">
         <Routes>
-          <Route path="/" element={WithAuthRedirect(ProfileContainer)} />
-          <Route path="/profile/:userId" element={WithAuthRedirect(ProfileContainer)} />
-          <Route path="/dialogues" element={WithAuthRedirect(DialoguesContainer)} />
+          <Route path="/" element={WithAuthRedirect(WithSuspense(ProfileContainer))} />
+          <Route path="/profile/:userId" element={WithAuthRedirect(WithSuspense(ProfileContainer))} />
+          <Route path="/dialogues" element={WithAuthRedirect(WithSuspense(DialoguesContainer))} />
           <Route path="/users" element={WithAuthRedirect(UsersContainer)} />
           <Route path="/news" element={WithAuthRedirect(News)} />
           <Route path="/music" element={WithAuthRedirect(Music)} />
---
+++ src/hoc/withSuspense.tsx
@@ -0,0 +1,12 @@
+import React, { Suspense } from "react";
+import { Preloader } from "components/common/Preloader/Preloader";
+
+export const WithSuspense = (Component: any) => {
+  return (props: any) => {
+    return (
+      <Suspense fallback={<Preloader />}>
+        <Component {...props} />
+      </Suspense>
+    );
+  };
+};
